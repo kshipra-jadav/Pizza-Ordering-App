7687575e638aae826bea3d3ae47aa1e6035aa7dc
kshipra-jadav <jadavkshipra2@gmail.com>
2022-08-11 20:01:08 +0530

    frontend - fixed ingredients selection, add - add to cart button

--- client/src/components/PizzaCard.tsx
+++ client/src/components/PizzaCard.tsx
@@ -1,89 +1,50 @@
-import { FC, useEffect, useState } from "react"
+import { FC, useEffect, useState, MouseEvent } from "react"
 import { Button, Card, Checkbox, Col, Dropdown, Menu, MenuProps } from "antd"
 import 'antd/dist/antd.css'
+import { ShoppingCartOutlined } from "@ant-design/icons"
+import Select, { Options } from "react-select"
 
 import { Pizza } from "../types/Pizza"
 import "./pizzacard.styles.css"
 import veg from "../assets/veg.jpg"
 import non_veg from "../assets/non veg.png"
-import { CheckboxChangeEvent } from "antd/es/checkbox"
 
 interface PizzaCardProps {
 	item: Pizza
 }
 
 const PizzaCard: FC<PizzaCardProps> = ({ item }): JSX.Element => {
-	const [ visible, setVisible ] = useState(false)
 	const [ price, setPrice ] = useState(0)
-	const [ checked, setChecked ] = useState(true)
+	const [ pizzaDesc, setPizzaDesc ] = useState([])
+	const [ total, setTotal ] = useState(0)
 	
 	useEffect(() => {
 		setPrice(item.price)
 	}, [])
 	
-	const handleMenuClick: MenuProps['onClick'] = ({ key }) => {
-		setVisible(true)
-		console.log(`menu item clicked :- ${ key }`)
-		checked && changePrice(parseInt(key))
+	
+	const ingredients = [
+		{ value: "Onion", label: "Onion - 20₹", id: 1, pizza: item.name, additionalCost: 20 },
+		{ value: "Capsicum", label: "Capsicum - 10₹", id: 2, pizza: item.name, additionalCost: 10 },
+		{ value: "Olives", label: "Olives - 30₹", id: 3, pizza: item.name, additionalCost: 30 },
+		{ value: "Baby Corn", label: "Baby Corn - 50₹", id: 4, pizza: item.name, additionalCost: 50 },
+		{ value: "Jalapenos", label: "Jalapenos - 20₹", id: 5, pizza: item.name, additionalCost: 20 },
+	]
+	
+	
+	const handleSelect = (option: any) => {
+		let total = 0
+		option.forEach((item: any) => {
+			total += item.additionalCost
+		})
+		setTotal(total)
+		setPizzaDesc(option)
 	}
 	
-	const handleVisibleChange = (flag: boolean) => {
-		setVisible(flag)
+	const handleBtnClick = (e: MouseEvent<HTMLButtonElement>) => {
+		const toppings = []
+		
 	}
-	
-	const onChange = (e: CheckboxChangeEvent) => {
-		setChecked(!e.target.checked)
-	}
-	
-	const changePrice = (key: number): void => {
-		switch (key){
-			case 1:
-				setPrice((price + 20))
-				break
-			case 2:
-				setPrice((price + 30))
-				break
-			case 3:
-				setPrice((price + 10))
-				break
-			case 4:
-				setPrice((price + 50))
-				break
-			case 5:
-				setPrice((price + 20))
-				break
-		}
-	}
-	
-	const ingredients = (
-		<Menu
-			items={ [
-				{
-					key: "1",
-					label: <Checkbox onChange={onChange}>Onion - 20₹</Checkbox>
-				},
-				{
-					key: "2",
-					label: <Checkbox>Capsicum - 30₹</Checkbox>
-				},
-				{
-					key: "3",
-					label: <Checkbox>Olives - 10₹</Checkbox>
-				},
-				{
-					key: "4",
-					label: <Checkbox>Jalapeno - 50₹</Checkbox>
-				},
-				{
-					key: "5",
-					label: <Checkbox>Baby Corn - 20₹</Checkbox>
-				},
-			
-			] }
-			onClick={ handleMenuClick }
-		/>
-	)
-	
 	
 	return (
 		<>
@@ -113,14 +74,17 @@ const PizzaCard: FC<PizzaCardProps> = ({ item }): JSX.Element => {
 					</div>
 					
 					<div className="dropdown">
-						<Dropdown overlay={ ingredients }
-						          placement="bottomLeft"
-						          trigger={ [ "click" ] }
-						          onVisibleChange={ handleVisibleChange }
-						          visible={ visible }
-						>
-							<Button>Ingredients</Button>
-						</Dropdown>
+						<div className="select" style={ { width: "250px" } }>
+							
+							<Select options={ ingredients }
+							        closeMenuOnSelect={ false }
+							        isMulti={ true }
+							        onChange={ handleSelect }
+							        placeholder={ "Ingredients" }
+							/>
+						</div>
+						<Button type="ghost" shape="round" icon={ <ShoppingCartOutlined/> } size={ 'large' }
+						        onClick={ handleBtnClick }/>
 					</div>
 				
 				</Card>
